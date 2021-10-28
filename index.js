1903a3edfc4932806f1467954b2a08484d5a2a35
TazeenEhsan <tazeenehsan73@gmail.com>
2021-10-28 15:52:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors')
+
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+
+const port = 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // Get single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await servicesCollection.findOne(query);
+            // console.log('load user with id: ', id);
+            res.send(user);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            console.log('got new user', newService);
+            console.log('added user', result);
+            res.json(result);
+        });
+
+        // //UPDATE API
+        // app.put('/services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const updatedUser = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             name: updatedUser.name,
+        //             email: updatedUser.email
+        //         },
+        //     };
+        //     const result = await servicesCollection.updateOne(filter, updateDoc, options)
+        //     console.log('updating', id)
+        //     res.json(result)
+        // })
+
+        // // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+
+            console.log('deleting user with id ', result);
+            res.json(result);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running');
+});
+
+
+app.listen(port, () => {
+    console.log('running at', port);
+})
